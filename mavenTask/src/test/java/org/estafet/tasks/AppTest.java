072b1ba45720ebb295dffcb9c97c1115e2360933
Iliana Markova <iliana.markova@estafet.com>
2022-04-19 13:07:50 +0300

    add first faker test

--- mavenTask/src/test/java/org/estafet/tasks/AppTest.java
+++ mavenTask/src/test/java/org/estafet/tasks/AppTest.java
@@ -1,8 +1,10 @@
 package org.estafet.tasks;
 
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
@@ -12,10 +14,23 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    Faker faker = new Faker();
+
     @Test
+    @DisplayName("First Test")
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    @DisplayName("Second test: Assert random first and last name are not empty strings")
+    public void shouldAssertFirstAndLastNameAreNotEmpty()
+    {
+        String fName = faker.name().firstName();
+        String lName = faker.name().lastName();
+        assertFalse(fName.isEmpty() && fName.isBlank());
+        assertFalse(lName.isEmpty() && lName.isBlank());
     }
 
 
